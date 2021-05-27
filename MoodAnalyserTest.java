fd111532cbe2d2d0fc1be46e0608e75b22e6150e
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-27 11:41:41 +0530

    Test case to check for a sad message is added

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,16 +1,19 @@
 package com.moodAnalyserTest;
 
-import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+
 
 public class MoodAnalyserTest {
 
 	@Test
 	public void givenMessageReturnMood() {
 		
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("Enter message here");
+		MoodAnalyser moodAnalyser = new MoodAnalyser(); //Object of MoodAnalyser class
+		String mood = moodAnalyser.analyseMood("Enter sad message here"); 
+		Assert.assertEquals("SAD",mood); //compares the value of mood with "SAD" 
 		System.out.println(mood);
 	}
 
