8e6df709bc349469d1f9a62c908e26b402701246
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-27 12:01:52 +0530

    Test case to return HAPPY for anay given message initialized through constructor of the class

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -30,9 +30,18 @@ public class MoodAnalyserTest {
 	@Test
 	public void givenSadMessageReturnSad() {
 		
-		MoodAnalyser moodAnalyser = new MoodAnalyser("Enter message here"); //message is initialized through constructor
+		MoodAnalyser moodAnalyser = new MoodAnalyser("Enter sad message here"); //message is initialized through constructor
 		String mood = moodAnalyser.analyseMood(); 
 		Assert.assertEquals("SAD",mood); //compares the value of mood with "SAD" 
+		System.out.println(mood);
+	}
+	
+	@Test
+	public void givenAnyMessageReturnHappy() {
+		
+		MoodAnalyser moodAnalyser = new MoodAnalyser("Enter message here"); //message is initialized through constructor
+		String mood = moodAnalyser.analyseMood(); 
+		Assert.assertEquals("HAPPY",mood); //compares the value of mood with "SAD" 
 		System.out.println(mood);
 	}
 
